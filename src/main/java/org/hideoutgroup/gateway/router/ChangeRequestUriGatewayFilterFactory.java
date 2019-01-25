2569f16a530160894914a5fb056ee78dedd9841e
董文强 <826112946@qq.com>
2019-01-25 18:01:51 +0800

    aaa

--- src/main/java/org/hideoutgroup/gateway/router/ChangeRequestUriGatewayFilterFactory.java
+++ src/main/java/org/hideoutgroup/gateway/router/ChangeRequestUriGatewayFilterFactory.java
@@ -1,29 +1,56 @@
 package org.hideoutgroup.gateway.router;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractChangeRequestUriGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
-
+import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.Optional;
+import java.util.function.Function;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.containsEncodedParts;
+
 
 /**
- * 
- * @date 2019年01月24日
  * @version 1.0
+ * @date 2019年01月24日
  */
-public class ChangeRequestUriGatewayFilterFactory extends AbstractChangeRequestUriGatewayFilterFactory {
- private static final Logger LOGGER = LoggerFactory.getLogger(ChangeRequestUriGatewayFilterFactory.class);
+
+public class ChangeRequestUriGatewayFilterFactory extends AbstractChangeRequestUriGatewayFilterFactory<ChangeRequestUriGatewayFilterFactory.Config>  {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChangeRequestUriGatewayFilterFactory.class);
 
     public ChangeRequestUriGatewayFilterFactory() {
         super(Config.class);
     }
 
+    static final String ServiceQueryName = "service";
+
     @Override
-    protected Optional<URI> determineRequestUri(ServerWebExchange exchange, Object config) {
-       // exchange.getRequest()
-        return Optional.empty();
+    protected Optional<URI> determineRequestUri(ServerWebExchange exchange, Config config) {
+        ServerHttpRequest req = exchange.getRequest();
+        URI uri = req.getURI();
+        URI tempURI = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+        String service = req.getQueryParams().getFirst(ServiceQueryName);
+        boolean encoded = containsEncodedParts(uri);
+        URI mergedUrl = UriComponentsBuilder.fromUri(tempURI)
+                .host(service)
+                .port(null)
+                .replaceQueryParam(ServiceQueryName)
+                .build(encoded)
+                .toUri();
+        LOGGER.info("原本请求{},转换后的请求{}", uri, mergedUrl);
+        return Optional.of(mergedUrl);
     }
+
+    public GatewayFilter apply() {
+       return super.apply(new Config());
+   }
+
 
 
     public static class Config {
--- src/main/java/org/hideoutgroup/gateway/router/RouterConfig.java
+++ src/main/java/org/hideoutgroup/gateway/router/RouterConfig.java
@@ -2,10 +2,13 @@ package org.hideoutgroup.gateway.router;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
 
 /**
  * @author 董文强
@@ -17,19 +20,20 @@ public class RouterConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(RouterConfig.class);
 
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    public ChangeRequestUriGatewayFilterFactory changeRequestUriGatewayFilterFactory() {
+        return new ChangeRequestUriGatewayFilterFactory();
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, @Autowired ChangeRequestUriGatewayFilterFactory filterFactory) {
+
         return builder.routes()
-                .route(p -> {
-                    return p
-                            .path("/api")
-                            //.filters(f -> f.addRequestHeader("Hello", "World"))
-                            .uri("http://httpbin.org:80");
-                })
                 .route(p -> p
-                        .host("*.hystrix.com")
-                        .filters(f -> f.hystrix(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80")).
-                        build();
+                        .path("/api/*").or().path("/api").or().path("/api/")
+                        .filters(f -> f.rewritePath("/api", ""))
+                        .uri(URI.create("http://111.111.111.111"))
+                        .filter(filterFactory.apply()))
+                .build();
     }
 
 }
